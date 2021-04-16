c137f1961f9f2cc308d8b0833374eb9910acd9ea
Eric Dam <dam.eric3@gmail.com>
2021-04-15 23:04:47 -0700

    fixed cart price display

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -9,11 +9,11 @@ export default function CartItem(props) {
             </div>
             <div className="item-desc">
                 <h3>{props.name}</h3>
-                <p className="item-price" id="price-mobile">Price: ${props.price}.00</p>
+                <p className="item-price" id="price-mobile">Price: ${props.price}</p>
             </div>
             <div className="item-total">
                 <button onClick={() => props.remove(props.id)}>Remove</button>
-                <p className="item-price" id="price-desktop">Price: ${props.price}.00</p>
+                <p className="item-price" id="price-desktop">Price: ${props.price}</p>
             </div>
         </div>
     );
