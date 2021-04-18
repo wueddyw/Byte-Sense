265a5bf6b098b7323b06418fb128f591e9c590c3
Eric Dam <dam.eric3@gmail.com>
2021-04-18 16:59:07 -0700

    tested

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,20 +2,14 @@ import React from 'react';
 import '../../App.css';
 import HomeTopBanner from '../HomeTopBanner';
 import Navbar from '../Navbar';
-import TrackVisibility from 'react-on-screen'; 
-import ExpandingImage from '../ExpandingImage';
-import HomeAboutUsBar from '../HomeAboutUsBar';
 import HomeImageLinks from '../HomeImageLinks';
 import HomeProducts from '../HomeProducts';
 import Footer from "../Footer";
-import PaypalButton from '../PayPalButton';
-import ContactMe from '../Emailer';
 
 import { NewsContextProviderHome } from '../NewsContext';
 import { GetNews } from '../GetNews';
 
 import './Home.css';
-import zoomImage01 from '../../images/home/home-news.png';
 export default function Home() {
 
 
--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
-
-
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
@@ -15,7 +10,6 @@ ReactDOM.render(
         </Router>, 
         document.getElementById('root')
 );
->>>>>>> fd73128da5ff9ba83c8a231f7fc1938032a8fa57
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
